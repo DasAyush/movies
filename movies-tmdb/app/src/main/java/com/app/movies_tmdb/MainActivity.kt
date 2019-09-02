1629bc274f7ddf38e61ad15d4a35fcd764b3210a
Ayush Das <ayush@manch.app>
2019-09-02 13:38:44 +0530

    Initial commit

---
+++ movies-tmdb/app/src/main/java/com/app/movies_tmdb/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.app.movies_tmdb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.movies_tmdb.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
